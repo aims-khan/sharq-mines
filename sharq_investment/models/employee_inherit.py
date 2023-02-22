261368dcd8cdc971764f813b40fc88ef28ee2ae9
ahmadshah24 <122726057+ahmadshah24@users.noreply.github.com>
2023-02-22 10:13:52 +0430

    Add investment model and is_investor(in employee) (#3)
    * Add investment model
    
    * Resolve the state widget issue in statubar
    
    * add invesment model and adding a field is_invistor to employee modul
    
    * after resolve the given issues
    
    * after last resolve
    
    * Remove new line from employee_inherit file
    
    ---------
    
    Co-authored-by: aims-khan <zuhal.nader@gmail.com>

---
+++ sharq_investment/models/employee_inherit.py
@@ -0,0 +1,8 @@
+from odoo import models, fields, api
+
+
+class EmployeeInherit(models.Model):
+    _inherit= "hr.employee"
+    _description = 'sharq_investment.sharq_employee'
+
+    is_investor = fields.Boolean("Invistor")
