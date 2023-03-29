b94a7718bfb90c618e845b566b7e0c4aaaa3d416
ahmadshah24 <ahmadshahqand@gmail.com>
2023-03-29 14:17:37 +0430

    add project line

--- sharq_project/models/models.py
+++ sharq_project/models/models.py
@@ -8,5 +8,26 @@ class project(models.Model):
     _description = 'project project inherit'
  
     employee_ids=fields.One2many("hr.employee","project_id")
+    line_ids=fields.One2many("project.line","project_id")
     product=fields.Char("Mine Product")
+
+
+
+
+
+
+class ProjectLine(models.Model):
+    _name = 'project.line'
+    _description = 'sharq_project.project.line.description'
+    _rec_name="investor_id"
+
+    part=fields.Float("Part")
+
+    #relational fields
+    investor_id=fields.Many2one("res.partner")
+    project_id=fields.Many2one("project.project")
+    expense_id=fields.Many2one("sharq.expense")
+    sale_id=fields.Many2one("sharq_sales.sharq_sales")
+    
+    
 
